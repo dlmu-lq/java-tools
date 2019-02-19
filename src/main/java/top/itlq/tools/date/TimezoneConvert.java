8b4a53750f29dc0f6c78483e33121283f48123fe
dlmu-lq <1725055744@qq.com>
2019-02-19 15:01:56 +0800

    init project with some tools

---
+++ src/main/java/top/itlq/tools/date/TimezoneConvert.java
@@ -0,0 +1,21 @@
+package top.itlq.tools.date;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * 日期格式化（包含时区）
+ */
+public class TimezoneConvert {
+    @Test
+    public void test1() throws Exception{
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        System.out.println(sdf.format(d));
+    }
+}
---
+++ src/main/java/top/itlq/tools/replace/Test.java
@@ -0,0 +1,54 @@
+package top.itlq.tools.replace;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test {
+    public static void main(String... args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new FileReader("src/main/java/top/itlq/tools/replace/test"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/java/top/itlq/tools/replace/re"));
+        String a;
+        StringBuffer sb1 = new StringBuffer();
+        while ((a = br.readLine()) != null) {
+
+            a = a.replaceAll(" ", "");
+            if (a.startsWith("fname:")) {
+                BufferedReader template = new BufferedReader(new FileReader("src/main/java/top/itlq/tools/replace/template"));
+                String templateLine;
+
+                while ((templateLine = template.readLine()) != null) {
+                    templateLine = templateLine.replaceAll(" ", "");
+                    Matcher matcher = Pattern.compile("(.*?):\\{\\{(.*?)\\}\\}").matcher(templateLine);
+                    Matcher matcherLine = Pattern.compile("fname:\"(.*?)\"").matcher(a);
+                    StringBuffer sbNew = new StringBuffer();
+                    while (matcherLine.find()) {
+                        while (matcher.find()) {
+                            if (matcherLine.group(1).equals(matcher.group(1))) {
+
+                                matcherLine.appendReplacement(sbNew, "fname:\"" + matcher.group(2) + "\",");
+
+                            }
+                        }
+                    }
+                    matcherLine.appendTail(sbNew);
+                    a = sbNew.toString();
+                }
+            }
+            bw.write(a+ "\r\n");
+        }
+        bw.flush();
+        bw.close();
+//        String string = sb1.toString();
+////        Matcher matcher = Pattern.compile(">\\s+<").matcher(string);
+////
+////        while (matcher.find()) {
+////            matcher.appendReplacement(sb, "><");
+////        }
+//        matcher.appendTail(sb);
+
+
+
+    }
+}
